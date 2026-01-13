17753f91088809ed5f97b021111120f1f1a52433
llovy0429 <96804582+llovy0429@users.noreply.github.com>
2026-01-12 21:17:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
 // config.js
 const datos = [
     { fecha: '2026-01-12', texto: "Como un testimonio de cada día de esta increíble aventura, he reunido estas pequeñas sensaciones. Cada una guarda un pensamiento, un agradecimiento, un recuerdo o un deseo para nuestro futuro. No son solo postales, son páginas sueltas de nuestra historia, un recordatorio de que, incluso en lo cotidiano, hay magia cuando estás tú. Para ti, en quien mi mundo encuentra su razón de ser. Con todo mi amor, ahora y siempre." },
+    { fecha: '2026-01-13', texto: "Un nuevo día, una nueva página. Las postales empiezan desde hoy porque no necesito esperar a una ocasión especial para tener un detalle y hacerte saber lo mucho que te quiero y lo feliz que soy contigo. Que este día te trate con la misma dulzura que tu tratas a mi corazón, frente al caos elige una sonrisa, que el trabajo no nuble tu brillo, que tengas un Hermoso Día." },
+    { fecha: '2026-01-14', texto: "lorem ipsum17" },
+    { fecha: '2026-01-15', texto: "lorem ipsum16" },
+    { fecha: '2026-01-16', texto: "lorem ipsum17" },
+    { fecha: '2026-01-17', texto: "lorem ipsum16" },
+    { fecha: '2026-01-18', texto: "lorem ipsum17" },
+    { fecha: '2026-01-19', texto: "lorem ipsum16" },
+    { fecha: '2026-01-20', texto: "lorem ipsum17" },
+    { fecha: '2026-01-21', texto: "lorem ipsum16" },
+    { fecha: '2026-01-22', texto: "lorem ipsum17" },
+    { fecha: '2026-01-23', texto: "lorem ipsum16" },
+    { fecha: '2026-01-24', texto: "lorem ipsum17" },
+    { fecha: '2026-01-25', texto: "lorem ipsum16" },
+    { fecha: '2026-01-26', texto: "lorem ipsum17" },
+    { fecha: '2026-01-27', texto: "lorem ipsum16" },
+    { fecha: '2026-01-28', texto: "lorem ipsum17" },
     { fecha: '2026-01-29', texto: 'Quiero confesarte que este mes ha tenido días tan duros como hermosos, verte tan desconectada de mi me partía el alma, intentar día tras día llegar a ti que poco a poco ibas cediendo pero cuando me dabas un stop lloraba por dentro, me hacía pedazos, pero la constancia está danndo frutos, el proceso ha sido hermoso, estamos aprendiendo de nuestro pasado, viviendo el presente y construyendo el futuro, yo me siento super feliz, espero que tu tambien lo estés. Feliz 1er mes de reconexión de nuestra relación.' },
     { fecha: '2026-01-30', texto: 'A veces pienso en como seria estar con alguien "más fácil". Alguien sin nuestras heridas, sin nuestros complejos. Y la verdad es que probablemente sería más simple, menos peleas, menos trabajo, pero tambien seria menos profundo, menos real, menos transformador. Porque lo fácil no nos hace crecer, lo difícil si, y contigo he crecido de formas que nunca imaginé. Así que aunque sea trabajo duro, es el trabajo que elijo. Porque contigo me estoy convirtiendo en la mejor version de mi.' },
     { fecha: '2026-01-31', texto: "lorem ipsum4" },
@@ -37,6 +53,7 @@ const fondos = [
         'src/naranja/fondo16.jpg'
 
     ];
+
 
 
 
